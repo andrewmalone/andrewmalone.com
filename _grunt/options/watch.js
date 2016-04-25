59ca97c1209d6a9539d62492fde7d6ffb4e2880d
andrewmalone <andrew@andrewmalone.com>
2016-04-25 07:18:31 -0400

    Initial setup for new collection

--- _grunt/options/watch.js
+++ _grunt/options/watch.js
@@ -2,14 +2,17 @@ module.exports = {
 	livereload: {
         files: [
             '_config.yml',
-            'index.html',
             '_layouts/**',
             '_posts/**',
             '_drafts/**',
             '_includes/**',
             'css/**',
             'christmas/**',
-            '_data/**'
+            '_data/**',
+            '_gear/**',
+            '*.html',
+            '*.md',
+            '**/*.md'
         ],
         tasks: ['sass', 'autoprefixer', 'jshint', 'shell:jekyllBuild'],
         options: {
