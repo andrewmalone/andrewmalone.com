461ab6790c2443582b249bc476fea7fd97922dca
andrewmalone <andrew@andrewmalone.com>
2016-04-30 14:08:24 -0400

    Add preview mode for grunt

--- _grunt/options/shell.js
+++ _grunt/options/shell.js
@@ -5,6 +5,9 @@ module.exports = {
     jekyllPublish: {
         command: 'jekyll build'
     },
+    jekyllPreview: {
+	  	command: 'jekyll build --config _config.yml,_config-dev.yml'
+    },
     publish: {
 	    command: 'rsync -avz --delete -e "ssh -p 2222" ~/Repositories/andrewmalone.com/_site/ amalone@kateleary.net:~/public_html/andrewmalone.com/'
     }
--- _grunt/tasks.js
+++ _grunt/tasks.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
 	grunt.registerTask('default', ['sass', 'autoprefixer', 'jshint', 'shell:jekyllBuild', 'connect', 'watch']);
+	grunt.registerTask('preview',['sass', 'autoprefixer', 'shell:jekyllPreview', 'connect', 'watch'])
 	grunt.registerTask('publish', ['sass', 'autoprefixer', 'shell:jekyllPublish', 'shell:publish']);
 };
