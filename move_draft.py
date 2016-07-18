383edd3ac38abc5f7cc3745c415444f11a108dca
andrewmalone <andrew@andrewmalone.com>
2016-07-18 16:18:09 -0400

    Add option for video conversion

--- move_draft.py
+++ move_draft.py
@@ -4,8 +4,16 @@ import subprocess
 import os
 from datetime import datetime
 
-draft_file = sys.argv[1]
-# draft_file = "_drafts/uke-ideas/Variations in c.md"
+draft_file = None
+video = False
+for var in sys.argv[1:]:
+    if var[0] == "-":
+        if var == "-v":
+            video = True
+    else:
+        draft_file = var
+	
+
 today = datetime.now().strftime("%Y-%m-%d")
 
 # find if there is an audio file to deal with and check for categories
@@ -41,11 +49,40 @@ for audio_file in audio_files:
     # os.rename(old, new)
     old_file = "audio/drafts/{}.wav".format(audio_file)
     new_file = "{}/{}.wav".format(audio_dir, audio_file)
+    new_file_base = new_file.split(".")[0]
     # move the audio files
     shutil.move(old_file, new_file)
     # shutil.move(old_file, new_file)
     # convert to mp3
     subprocess.call(["lame", "-V6", new_file])
+
+    # convert to video
+    if video is True:
+        command = [
+            'ffmpeg',
+            '-i',
+            new_file,
+            '-filter_complex',
+            '[0:a]showwaves=s=400x300:mode=cline:rate=25,format=yuv420p[vid]',
+            '-map',
+            '[vid]',
+            '-map',
+            '0:a',
+            '-codec:v',
+            'libx264',
+            '-crf',
+            '18',
+            '-preset',
+            'fast',
+            '-codec:a',
+            'aac',
+            '-strict',
+            '-2',
+            '-b:a',
+            '192k',
+            "{}.mp4".format(new_file_base)
+        ]
+        subprocess.call(command)
 
 # move the file to posts and rename it
 post_file = "_posts/{}{}-{}".format(category, today, draft_file.split("/")[-1].replace(" ", "-").lower())
